74b8288e8b118827d51e6023cfe285516bda6029
Lyssa Li <lyssa.li137@gmail.com>
2025-01-29 01:32:01 -0800

    top bar css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,25 @@
-<script>
+<script lang="ts">
   import "@fontsource/anton";
   import "@fontsource/lato";
 
   let { children } = $props();
+
+  function swapSelectedNav(e: Event) {
+    document.querySelectorAll("nav a[data-cur='y']").forEach((el) => {
+      el.setAttribute("data-cur", "n");
+    });
+    const a = e.target as HTMLAnchorElement;
+    a.setAttribute("data-cur", "y");
+  }
 </script>
 
 <div id="everything-bagel" class="fl col">
-  <div class="sticky">
-    <nav class="row">
-      <a href="/">home</a>
-      <a href="/about">about</a>
-      <a href="/portfolio">portfolio</a>
-      <a href="/resume">resume</a>
-    </nav>
-  </div>
+  <nav class="row">
+    <a href="/" onclick={(e) => swapSelectedNav(e)} data-cur="y">home</a>
+    <a href="/about" onclick={(e) => swapSelectedNav(e)}>about</a>
+    <a href="/portfolio" onclick={(e) => swapSelectedNav(e)}>portfolio</a>
+    <a href="/resume" onclick={(e) => swapSelectedNav(e)}>resume</a>
+  </nav>
 
   <main class="col">
     {@render children()}
@@ -23,17 +29,14 @@
 </div>
 
 <style>
-  nav {
-    justify-content: space-around;
-  }
-
   main {
     flex-grow: 1;
     width: 100%;
     max-width: 800px;
+    margin-top: 5rem;
     margin-left: auto;
     margin-right: auto;
-    padding: 1em;
+    padding: 1rem;
   }
 
   #everything-bagel {
@@ -41,11 +44,41 @@
     height: 100vh;
   }
 
+  nav {
+    justify-content: space-around;
+    position: fixed;
+    width: 100%;
+    z-index: 100;
+  }
+  nav a {
+    color: var(--ac);
+    background-color: var(--bg);
+    border-bottom: var(--ac) 1px solid;
+    height: 100%;
+    flex-grow: 1;
+    font-family: "Anton", sans-serif;
+    font-stretch: expanded;
+    text-align: center;
+    letter-spacing: 0.2rem;
+    transition: padding-top 0.2s;
+  }
+  nav a:hover {
+    padding-top: 2.4rem;
+  }
+  nav a[data-cur="y"] {
+    color: var(--text);
+    border-bottom: var(--text) 1px solid;
+  }
   #foot {
     text-align: center;
+    border-top: var(--ac) 1px solid;
+    color: var(--ac);
+    font-family: "Anton", sans-serif;
+    font-stretch: expanded;
+    letter-spacing: 0.2rem;
   }
-
-  #foot, nav {
+  #foot,
+  nav a {
     padding: 1rem;
   }
 </style>
