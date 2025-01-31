394b97973a53fd0d3b67e52f37118b59849562fb
Lyssa Li <lyssa.li137@gmail.com>
2025-01-30 20:23:55 -0800

    fixing typos

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,21 +4,21 @@
 
   <p>
     I am currently pursuing a B.S. in Computer Science: Game Design at
-    University of California: Santa Cruz and am hoping to get a Masters in the
+    University of California: Santa Cruz and am hoping to get a master's in the
     same field with a focus on experience design in the future.
   </p>
   <p>
     Games have always been a fascination for me, especially their communicative
-    experiences. I very much enjoy watching people navigate through games
+    experiences. I very much enjoy watching people navigate through game
     environments and interacting with game characters. Their thoughts about what
-    the game convey and their connections with these in game worlds are very
-    important to me. I too would like to use games as a form of expression and
+    the game conveys and their connections with these in-game worlds are very
+    important to me. I, too, would like to use games as a form of expression and
     communcation.
   </p>
   <p>
-    If you would like to see what I have made so far, check out the projects
+    If you would like to see what I have made so far, check out the Projects
     tab. If you would like to see a better breakdown of my skills, experiences,
-    and previous work, check out the resume tab. Thank you for visiting :D
+    and previous work, check out the Resume tab. Thank you for visiting :D
   </p>
 </div>
 <div id="silly-things">
