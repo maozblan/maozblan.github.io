fd7f69d246cbe82da843a8354cec8381b375f981
maozblan <lyssa.li137@gmail.com>
2026-06-18 08:05:35 -0700

    deleting art folder that i forgot about

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,21 +1,6 @@
-interface ResumeBlock {
-  title: string;
-  sub1?: string;
-  sub2: string;
-  text: string[];
-  list: boolean;
-}
-
 interface Project {
   title: string;
-  year: number;
   icon: string;
   type: string;
   tags: string[];
-  tagline: string;
-}
-
-interface Link {
-  href: string;
-  text: string;
 }
--- src/routes/portfolio/portfolio.ts
+++ src/routes/portfolio/portfolio.ts
@@ -3,27 +3,21 @@ import { img } from "$lib/img";
 export const portfolio: { [key: string]: Project } = {
   stitchlings: {
     title: "Stitchlings",
-    year: 2026,
     icon: img.stitch_icon,
     type: "game",
     tags: ["c#", "unity", "art", "ui"],
-    tagline: "fishing poker...?",
   },
   fishing: {
     title: "FiSHiNG",
-    year: 2026,
     icon: img.fishing_icon,
     type: "game",
     tags: ["c#", "unity", "art", "ui"],
-    tagline: "fishing poker...?",
   },
   fireworks: {
     title: "L-system fireworks",
-    year: 2025,
     icon: img.fw_icon,
     type: "procedural art",
     tags: ["p5", "javascript"],
-    tagline: "something small, something silly",
     // links: [
     //   {
     //     href: "https://maozblan.github.io/cmpm169/experiment3/index.html",
@@ -37,11 +31,9 @@ export const portfolio: { [key: string]: Project } = {
   },
   coffeeForTheGone: {
     title: "coffee for the gone",
-    year: 2024,
     icon: img.cftg_icon,
     type: "game",
     tags: ["typescript", "html5"],
-    tagline: "why don't we chill over a cup of otherworldly coffee",
     // links: [
     //   {
     //     href: "https://maozblan.github.io/cmpm-121-demo-1",
@@ -55,11 +47,9 @@ export const portfolio: { [key: string]: Project } = {
   },
   scaramuccia: {
     title: "scaramuccia",
-    year: 2025,
     icon: img.scara_icon,
     type: "desktop wallpaper",
     tags: ["design", "wallpaper"],
-    tagline: "try, but the screen cannot save you",
     // links: [
     //   {
     //     href: "https://steamcommunity.com/sharedfiles/filedetails/?id=3403015347",
@@ -73,11 +63,9 @@ export const portfolio: { [key: string]: Project } = {
   },
   linesOfSleight: {
     title: "lines of sleight",
-    year: 2024,
     icon: img.los_icon,
     type: "game",
     tags: ["unity3D", "c#", "ui"],
-    tagline: "it all comes down to the draw",
     // links: [
     //   {
     //     href: "https://violets321.itch.io/line-of-sleight",
@@ -91,11 +79,9 @@ export const portfolio: { [key: string]: Project } = {
   },
   smokescreenAndMirrors: {
     title: "smokescreen & mirrors",
-    year: 2024,
     icon: img.sam_icon,
     type: "game",
     tags: ["javascript", "phaser"],
-    tagline: "meow meow meow, pew pew pew",
     // links: [
     //   {
     //     href: "https://maozblan.github.io/smokescreen-and-mirrors/",
