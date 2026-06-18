9a38aec2769cf5da92d19ddd8b9043c806d2143e
maozblan <lyssa.li137@gmail.com>
2026-06-18 08:00:54 -0700

    fixing node dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,9 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const dev = process.env.NODE_ENV === 'development';
+const repo = process.env.GITHUB_REPOSITORY?.split('/')[1];
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
@@ -13,7 +16,16 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 
 		// switching to adapter-static for github pages deployment https://www.captaincodeman.com/deploy-a-sveltekit-app-to-github-pages
-		adapter: adapter()
+		adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: undefined,
+      precompress: false,
+      strict: true
+    }),
+	    paths: {
+      base: dev ? '' : `/${repo}`
+    }	
 	}
 };
 
